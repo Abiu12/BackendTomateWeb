5148cad699f86cab4fa43fe46c86e3b50b09daca
Abiu12 <callejas195@gmail.com>
2024-03-20 18:51:48 -0600

    feat: Create plague

--- controllers/plague.controller.js
+++ controllers/plague.controller.js
@@ -3,7 +3,14 @@ export class PlagueController{
     static async getAll(req,res){}
     static async getById(req,res){}
     static async create(req,res){
-        
+        const {
+            name,
+            nameScientific,
+            recommendations,
+            actions
+        } = req.body
+        const result = await PlagueModel.create({input:{name,nameScientific,recommendations,actions}})
+        res.json({message: "Plaga registrada"})
     }
     static async update(req,res){}
     static async delete(req,res){}
