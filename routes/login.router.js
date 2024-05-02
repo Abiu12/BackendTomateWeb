e133788a201c5cd47b5ce4df0c4ff351ba738957
Abiu12 <callejas195@gmail.com>
2024-05-01 22:55:23 -0600

    Fix: get to post in getDataByUsername

--- routes/login.router.js
+++ routes/login.router.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { LoginController } from "../controllers/login.controller.js";
 
-export const LoginRouter = Router ()
+export const LoginRouter = Router();
 
-LoginRouter.post('/',LoginController.login)
-LoginRouter.post('/check_email_existence',LoginController.checkEmailExistence)
-LoginRouter.post('/send_recovery_email',LoginController.sendEmail)
-LoginRouter.patch('/changePassword',LoginController.changePassword)
-LoginRouter.get('/getDataByUsername',LoginController.getDataByUsername)
+LoginRouter.post("/", LoginController.login);
+LoginRouter.post("/check_email_existence", LoginController.checkEmailExistence);
+LoginRouter.post("/send_recovery_email", LoginController.sendEmail);
+LoginRouter.patch("/changePassword", LoginController.changePassword);
+LoginRouter.post("/getDataByUsername", LoginController.getDataByUsername);
