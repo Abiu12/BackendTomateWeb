334f4663e0fd8c2ad9ec37f51adc9429a181ae7f
Abiu12 <callejas195@gmail.com>
2024-06-01 23:34:03 -0600

    Fix: Analizar imagen en aplicacion web

--- routes/analizeimage.route.js
+++ routes/analizeimage.route.js
@@ -2,11 +2,8 @@ import { Router } from "express";
 import { AnalizeImageController } from "../controllers/analizeimage.controller.js";
 
 export const AnalizeImageRouter = Router();
-
-AnalizeImageRouter.post("/:idBed/:idUser", AnalizeImageController.detected);
-
 AnalizeImageRouter.post("/web/:idBed", AnalizeImageController.detectedWeb);
-
+AnalizeImageRouter.post("/:idBed/:idUser", AnalizeImageController.detected);
 AnalizeImageRouter.post(
   "/:tokenNotification",
   AnalizeImageController.detectedGuest
